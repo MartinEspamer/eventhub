43089152a6a133ac797317527a4953630edaad85
baucuenca <jnb.cuenca1507@gmail.com>
2025-05-26 00:21:36 -0300

    Agregado templatetag que devuelve el promedio de calificaciones y renderizado en el detalle del evento

--- rating/templatetags/rating_tags.py
+++ rating/templatetags/rating_tags.py
@@ -1,5 +1,8 @@
 from django import template
 from ..forms import RatingForm
+from ..models import Rating
+from django.db.models import Avg, Count
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -13,3 +16,18 @@ def render_rating(context, event):
         "ratings": ratings,
         "form": form,
     }
+
+@register.simple_tag
+def event_rating_avg(event):
+    sum = Rating.objects.filter(event=event).aggregate(
+        avg=Avg('rating'),
+        count=Count('id')
+    )
+    avg = sum['avg']
+    count = sum['count']
+
+    if count==0:
+        return "Sin reseñas"
+
+    avg_formatted = f"{avg:.1f}".replace('.', ',')
+    return mark_safe(f"<strong>{avg_formatted}</strong> <i class='bi bi-star-fill text-warning'></i> ({count} Reseñas)")
